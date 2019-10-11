a02a4ddc4695b4c3ee08ce32d1b4699fe5c14bca
Junyan Wang <jwan0102@student.monash.edu>
2019-10-11 23:38:41 +1100

    add vue form and mixin

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase | to-lowercase}}</p>
+                <hr>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li 
+                        v-for="(fruit,index) in filteredFruits" 
+                        :key="index">
+                        {{fruit}}
+                    </li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin.js';
+    export default {
+        mixins: [fruitMixin]
+    }
+</script>
+
+<style>
+
+</style>
